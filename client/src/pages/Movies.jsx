92eb0140a4e6179d82423de5ba27e9a8bcc09ab5
Aditya-Borhade <adityaborhade793@gmail.com>
2025-07-26 23:55:49 +0530

    processing with movie page

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import {dummyTrailers} from '../assets/assets.js'
+import {dummyShowsData } from '../assets/assets'
+import MovieCard from '../components/MovieCard';
 const Movies = () => {
-  return (
-    <div>
-      <h1>hello from movie</h1>
-      {dummyTrailers.map((movie)=>(
-          <img src={movie.image} alt="" />
-      ))}
+  return dummyShowsData.length>0 ? (
+    <div className='relative my-40 mb-40 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
+      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+      <div className='flex flex-wrap max-sm:justify-center gap-8 '>
+        {dummyShowsData.map((movie)=>(
+          <MovieCard movie={movie} key={movie._id}/>
+        ))}
+      </div>
     </div>
+  ):(
+     <div>no data</div>
   )
 }
 
