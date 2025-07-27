736a27e30d9685ad288b96ff0af6111f82f4bb19
Aditya-Borhade <adityaborhade793@gmail.com>
2025-07-27 12:07:20 +0530

    working on moviedetails page

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -12,7 +12,7 @@ const Movies = () => {
       <h1 className='text-lg font-medium my-4'>Now Showing</h1>
       <div className='flex flex-wrap max-sm:justify-center gap-8 '>
         {dummyShowsData.map((movie)=>(
-          <MovieCard movie={movie} key={movie._id}/>
+          <MovieCard movie={movie} key={movie._id} />
         ))}
       </div>
     </div>
