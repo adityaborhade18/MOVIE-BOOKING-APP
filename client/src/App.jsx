0c67f08c8c9192b49663ef6bcce721d9f59d6e9b
Aditya-Borhade <adityaborhade793@gmail.com>
2025-07-31 16:43:50 +0530

    admin sidebar and navbar completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,9 +32,9 @@ const App=()=>{
 
           <Route path="/admin/*" element={<Layout/>}>
               <Route index element={<Dashboard/>}/>
-              <Route path='/add-shows' element={<AddShows/>}/>
-              <Route path='/list-shows' element={<ListShows/>}/>
-              <Route path='/list-bookings' element={<ListBookings/>}/>
+              <Route path='add-shows' element={<AddShows/>}/>
+              <Route path='list-shows' element={<ListShows/>}/>
+              <Route path='list-bookings' element={<ListBookings/>}/>
           </Route>
 
       </Routes>
