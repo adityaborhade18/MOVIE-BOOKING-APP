f45746989c1ca74dc9751f42b7f6185980431d93
Aditya-Borhade <adityaborhade793@gmail.com>
2025-08-05 19:45:06 +0530

    Add server setup and Inngest integration for background workflows

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './configs/dbConfig.js';
+import {clerkMiddleware} from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+
+
+const app=express();
+const port =process.env.PORT || 3000;
+
+await connectDB()
+.then(()=>{
+    console.log('connected to database');
+})
+.catch((err)=>{
+    console.log('error connecting to database',err);
+})
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware())
+
+
+app.get('/',(req,res)=>{
+   res.send("hello from movies")
+})
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.listen(port , ()=>{
+    console.log(`server is running at http://localhost:${port}`);
+})
