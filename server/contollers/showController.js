61179273c6eec2c0b11989999a434ffdf928d09b
Aditya-Borhade <adityaborhade793@gmail.com>
2025-08-11 19:21:17 +0530

    added routes to get shows

--- server/contollers/showController.js
+++ server/contollers/showController.js
@@ -83,3 +83,39 @@ export const addShow=async()=>{
         res.json({success:false, message:error.message});
     }
 }
+
+// api to get all shows from database
+export const getShows=async()=>{
+    try{
+       const shows=await Show.find({showDateTime:{$gte:new Date()}}).populate('movie').sort({showDateTime:1});
+
+       const uniqueShows=new Set(shows.map(show=>show.movie));
+       res.json({success:true,shows:Array.from(uniqueShows)});
+    }catch(error){
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
+}
+
+//api to get one show from database
+export const getShow=async(req,res)=>{
+    try{
+        const movieId=req.params;
+        const shows=await Show.find({movie:movieId , showDateTime:{$gte:new Date()}})
+
+        const movie=await Movie.findById(movieId);
+        const dateTime={};
+
+        shows.forEach((show)=>{
+            const date=show.showDateTime.toISOString().split("T")[0];
+            if(!dateTime[date]){
+                dateTime[date]=[];
+            }
+            dateTime[date].push({time:show.showDateTime,showId:show._id});
+        })
+        res.json({success:true,movie,dateTime})
+    }catch(error){
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
+}
