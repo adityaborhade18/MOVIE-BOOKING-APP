fbd6725f2e0e06b53ca444938abfa47278a7a2e8
Aditya-Borhade <adityaborhade793@gmail.com>
2025-08-12 17:36:18 +0530

    created apis for the admin section

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
 
 
 
@@ -35,6 +36,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use('/api/show',showRouter);
 app.use('/api/booking', bookingRouter);
+app.use('/api/admin', adminRouter);
 
 
 
